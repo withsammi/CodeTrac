ecb3c364407cbf3c6cbb4f55a47006c0b4056508
Aditya Rai <106179911+withsammi@users.noreply.github.com>
2025-04-18 12:57:24 +0530

    Delete Family Travel Tracker directory

--- Family Travel Tracker/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import createUser from "../controller/user.controller.js";
-
-const userRouter = express.Router()
-
-userRouter.post("/greet",createUser)
-
-export default userRouter;
